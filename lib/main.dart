cbe732e1c5a07dcd749509c7f69521125b34a996
Aadithye N <aadithyen@gmail.com>
2018-10-12 04:33:21 +0530

    Theme and Layout 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,14 @@ void main() => runApp(NoteStrike());
 class NoteStrike extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: "Note Strike", home: Body());
+    return new MaterialApp(
+      title: "Note Strike",
+      home: Body(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueGrey[700],
+      )
+    );
   }
 }
 
@@ -34,6 +41,7 @@ class BodyState extends State<Body> {
             fontWeight: FontWeight.bold
           ),
         ),
+       // backgroundColor: Color.fromRGBO(30, 4, 40, 1.0),
       ),
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.add),
@@ -42,12 +50,18 @@ class BodyState extends State<Body> {
           print("Pressed");
         }
       ),
-      body: new ListView.builder(
-        padding: new EdgeInsets.all(16.0),
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return new NoteCard();
-        },
+      body: new Container(
+        child: ListView.builder(
+          padding: new EdgeInsets.all(16.0),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return new Container(
+              child: NoteCard(),
+              padding: EdgeInsets.only(bottom:10.0),
+              );
+          },
+        ),
+       // color: Colors.grey[900],
       )
     );
   }
--- lib/notecard.dart
+++ lib/notecard.dart
@@ -11,18 +11,41 @@ class NoteCardState extends State<NoteCard> {
   @override
     Widget build(BuildContext context) {
       return new Card(
-        child: new Container(
-          child: new Column(
-            children: <Widget>[
-              new ListTile(
-                leading: new Icon(
-                  Icons.bookmark,
-                  color: Colors.blue,
+        child: new InkWell(
+          child: new Container(
+            child: new Column(
+              children: <Widget>[
+                new ListTile(
+                  leading: new Icon(
+                    Icons.bookmark,
+                    color: Colors.blueAccent,
+                  ),
+                  title: new Text("Text Title")
                 ),
-                title: new Text("Text Title")
-              )
-            ],
+                new ButtonTheme.bar(
+                  child: new ButtonBar(
+                    children: <Widget>[
+                      new IconButton(
+                        onPressed: () {
+
+                        },
+                        icon: Icon(Icons.edit),
+                      ),
+                      new IconButton(
+                        onPressed: () {
+
+                        },
+                        icon: Icon(Icons.delete),
+                      )
+                    ]
+                  ),
+                )
+              ],
+            ),
           ),
+          onTap: () {
+            print("Pressed on Card");
+          },
         )
       );
     }
